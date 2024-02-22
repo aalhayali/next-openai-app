77c1cd9232f932ce5c9ccbc257dde181a090c42f
Abdullah Al-Hayali <abdullah.w.alhayali@gmail.com>
2024-02-22 18:39:22 -0500

    Initial commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,24 +1,24 @@
-import OpenAI from 'openai';
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
-
+ 
 // Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
-});
-
+const client = new OpenAIClient(
+  'https://dice-openai.openai.azure.com',
+  new AzureKeyCredential(process.env.AZURE_OPENAI_API_KEY!),
+);
+ 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
-
+ 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
+ 
+  // Ask Azure OpenAI for a streaming chat completion given the prompt
+  const response = await client.streamChatCompletions(
+    'dice-scribe-mvp',
     messages,
-  });
-
+  );
+ 
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
   // Respond with the stream
